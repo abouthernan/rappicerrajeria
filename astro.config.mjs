00d8f32f2532696c1b50785614c4f78462c51c39
Cristhian Melo <rxtsel@hotmail.com>
2023-01-20 16:18:24 -0500

    feat(dev): add image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import react from '@astrojs/react'
-import image from '@astrojs/image'
+import react from '@astrojs/react';
+import image from '@astrojs/image';
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rappicerrajeria.vehement.co',
   integrations: [react(), image()]
-})
+});
